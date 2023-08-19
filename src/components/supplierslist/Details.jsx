2f5566f645997b8f5de584c13f167c431da32164
j <137029997+Jagacool@users.noreply.github.com>
2023-08-19 14:32:27 +0530

    Update Details.jsx

--- src/components/supplierslist/Details.jsx
+++ src/components/supplierslist/Details.jsx
@@ -7,7 +7,26 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
 function Details() {
-  // ...
+  const [supplier, setSupplier] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    axiosInstance.get(`/${id}`)
+      .then(response => setSupplier(response.data))
+      .catch(error => console.error(error));
+  }, [id]);
+
+  const card = (
+    <CardContent>
+      {/* ... (Rest of the card content) */}
+    </CardContent>
+  );
+
+  return (
+    <Box sx={{ minWidth: 275, m: "15rem" }}>
+      <Card variant="outlined">{card}</Card>
+    </Box>
+  );
 }
 
 export default Details;
