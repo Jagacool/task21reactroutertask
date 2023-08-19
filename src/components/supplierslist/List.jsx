f5449c040f725075fb51bce93dc6b29f4be1edf1
j <137029997+Jagacool@users.noreply.github.com>
2023-08-19 14:32:54 +0530

    Update List.jsx

--- src/components/supplierslist/List.jsx
+++ src/components/supplierslist/List.jsx
@@ -10,7 +10,19 @@ import TableRow from '@mui/material/TableRow';
 import Button from '@mui/material/Button';
 
 function List() {
-  // ...
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axiosInstance.get()
+      .then(response => setList(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <Box sx={{ bgcolor: '#cfe8fc', p: "5rem" }} align="center">
+      {/* ... (Rest of the List component content) */}
+    </Box>
+  );
 }
 
 export default List;
