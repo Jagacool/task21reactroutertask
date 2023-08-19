6070ab82b18ef63d02105cfccc041a067cc067ae
j <137029997+Jagacool@users.noreply.github.com>
2023-08-19 14:20:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import DrawerAppBar from './components/navbar/Navbar';
 import HomePage from './components/homepage/HomePage';
-import DrawerAppBar from './components/navbar/Navbar'
 import List from './components/supplierslist/List';
-import Add from './components/addsupplier/AddItem';
 import Details from './components/supplierslist/Details';
+import AddItem from './components/addsupplier/AddItem';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<DrawerAppBar/>}>
-        <Route index path='/' element={<HomePage/>}/>
-        <Route index path='Home' element={<HomePage/>}/>
-        <Route path='/Suppliers' element={<List/>}/>
-        <Route path='Suppliers/:id' element={<Details/>}/>
-        <Route index path='Add' element={<Add/>}/>
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DrawerAppBar />}>
+          <Route index path="/" element={<HomePage />} />
+          <Route path="/Suppliers" element={<List />} />
+          <Route path="/Suppliers/:id" element={<Details />} />
+          <Route path="/Add" element={<AddItem />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
