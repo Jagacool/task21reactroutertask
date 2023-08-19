3f56f0c041ba985b0af3bdb3770983f64fc6df58
j <137029997+Jagacool@users.noreply.github.com>
2023-08-19 14:31:14 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,12 +18,43 @@ import { Outlet } from 'react-router-dom';
 const drawerWidth = 240;
 const navItems = ['Home', 'Suppliers', 'Add'];
 
-function DrawerAppBar(props) {
-  // ...
+function Navbar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        <Button href="/" variant="text" sx={{ color: 'black' }}>Northwind JSON Server</Button>
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <>
+      {/* ... (Rest of the Navbar component code) */}
+    </>
+  );
 }
 
-DrawerAppBar.propTypes = {
+Navbar.propTypes = {
   window: PropTypes.func,
 };
 
-export default DrawerAppBar;
+export default Navbar;
